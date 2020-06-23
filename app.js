f66db4c4115ec7f60c9321944ab415ef59d3b5d7
MadsRF <madsrunefrederiksen@gmail.com>
2020-06-23 14:53:48 +0200

    added login html. problem with css

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 
+// Makes public folder static, so it's always accessable    
+app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
@@ -10,33 +12,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-// Makes public folder static, so it's always accessable    
-app.use(express.static("public"));
-
 
 // Routes 
+app.get(("/"), (req, res) => {
+    console.log("login");
+    return res.sendFile(__dirname + "/public/loginPage/loginPage.html");
+});
+
 app.get("/orderPage", (req, res) => {
     console.log("orderPage");
     return res.sendFile(__dirname + "/public/orderPage/orderPage.html");
 });
 
 app.get(("/itemPage"), (req, res) => {
+    console.log("itemPage");
     return res.sendFile(__dirname + "/public/itemPage/itemPage.html");
 });
 
 app.get(("/archivePage"), (req, res) => {
+    console.log("archivePage");
     return res.sendFile(__dirname + "/public/archivePage/archivePage.html");
 });
 
-app.get(("/"), (req, res) => {
-    return res.status(501).send({ response: "Not implmented yet" });
-});
 
 
-// global middelware sits between backend and frontend. works on all routes
+
+// Global middelware sits between backend and frontend. works on all routes
 const firebase = require("./routes/firebase.js");
 
-//set up routes with our server
+// Set up routes with our server
 app.use(firebase);
 
 
@@ -44,7 +48,7 @@ app.use(firebase);
 const port = process.env.PORT ? process.env.PORT : 8888;
 
 
-//eroor handling
+// Error handling on server upstart
 const server = app.listen(port, (error) => {
     if (error) {
         console.log("Error starting the server");
